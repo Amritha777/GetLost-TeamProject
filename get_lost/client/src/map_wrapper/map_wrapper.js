ac598274e03929e6d5839d56cffaa078af19de9d
Carlos Pereira <carlinhop@gmail.com>
2016-12-15 12:03:47 +0000

    MapWrapper created

---
+++ get_lost/client/src/map_wrapper/map_wrapper.js
@@ -0,0 +1,32 @@
+var MapWrapper = function(container, center, zoom){
+
+ this.googleMap = new google.maps.Map(container,
+
+ {
+   center: center,
+   zoom: zoom
+ }
+
+ );
+}
+
+MapWrapper.prototype = {
+
+ addMarker: function(coords){
+   var marker = new google.maps.Marker({
+     position: coords,
+     map: this.googleMap
+   })
+ },
+
+ addClickEvent: function(){
+   google.maps.event.addListener(this.googleMap, 'click', function(event){
+     var position = event.latLng;
+     console.log(this); 
+     this.addMarker(position);
+   }.bind(this))
+ }
+
+}
+
+module.exports = MapWrapper;
