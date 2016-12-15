44ed53943eeb011306f325a18b55a386bea0062e
David O'Leary <david.oleary174@gmail.com>
2016-12-15 11:26:28 +0000

    city tests passing

---
+++ get_lost/client/src/city/city.js
@@ -0,0 +1,9 @@
+var City = function(name, language, currency, coordinates) {
+  this.name = name;
+  this.language = language;
+  this.currency = currency;
+  this.coordinates = coordinates;
+};
+
+module.exports = City;
+
