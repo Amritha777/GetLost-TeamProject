1091b7dc72d09a4aca35bbd9888bc39a3b34e33d
Carlos Pereira <carlinhop@gmail.com>
2016-12-18 22:58:47 +0000

    Added infowindows on markers

--- get_lost/client/src/map_wrapper/map_wrapper.js
+++ get_lost/client/src/map_wrapper/map_wrapper.js
@@ -10,10 +10,25 @@ var MapWrapper = function(container, center, zoom){
 MapWrapper.prototype = {
 
  createMarker: function(place){
+
    var marker = new google.maps.Marker({
      position: {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()},
      map: this.googleMap
    });
+ 
+   var infowindow = new google.maps.InfoWindow({content: place.name});
+
+   marker.addListener('mouseover', function() {
+        
+       infowindow.open(this.googleMap, marker);
+     });
+
+   marker.addListener('mouseout',function(){
+    infowindow.close();
+   });
+
+
+
  },
 }
 
